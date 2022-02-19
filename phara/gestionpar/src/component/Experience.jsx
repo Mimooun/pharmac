c8d5d9663c4fd6d4ae4a782ff2248d5a38bd5803
Mimooun <83767866+Mimooun@users.noreply.github.com>
2022-02-19 17:26:21 +0100

    changement des donnes

--- phara/gestionpar/src/component/Experience.jsx
+++ phara/gestionpar/src/component/Experience.jsx
@@ -9,20 +9,20 @@ function Experience() {
                    <div className="text">Years Experience </div>
                </div>
                <div className="content">
-                   <div className="number">25</div>
-                   <div className="text">Years Experience </div>
+                   <div className="number">893</div>
+                   <div className="text">Medicament Invented  </div>
                </div>
                <div className="content">
-                   <div className="number">25</div>
-                   <div className="text">Years Experience </div>
+                   <div className="number">75</div>
+                   <div className="text">Awards Winned  </div>
                </div>
                <div className="content">
-                   <div className="number">25</div>
-                   <div className="text">Years Experience </div>
+                   <div className="number">673K</div>
+                   <div className="text">Happy Clients  </div>
                </div>
                <div className="content">
-                   <div className="number">25</div>
-                   <div className="text">Years Experience </div>
+                   <div className="number">751</div>
+                   <div className="text">Happy Clients  </div>
                </div>
            </div>
         
