feb1b2c0c5f97d072d60cfb3e26da3ab05775212
zakari64 <74094535+zakari64@users.noreply.github.com>
2022-02-24 10:55:44 +0100

    changed form layout

--- phara/gestionpar/src/components/Form.jsx
+++ phara/gestionpar/src/components/Form.jsx
@@ -10,109 +10,120 @@ import Button from "@mui/material/Button";
 import Stack from "@mui/material/Stack";
 import "../styles/form.css";
 
-export default function FormPropsTextFields() {
+function Form() {
   return (
     <Box
-      component="form"
-      sx={{
-        "& .MuiTextField-root": { m: 1, width: "25ch" },
-      }}
-      noValidate
-      autoComplete="off"
-    >
+    component="form"
+    sx={{
+      "& .MuiTextField-root": { m: 1, width: "40ch" },
+    }}
+    noValidate
+    autoComplete="off"
+  >
+    <section>
       <div className="max-width">
-        <div className="title">Commande</div>
-        <div className="textfield-container">
-          <div className="left">
-            <TextField
-              className="textfield"
-              required
-              id="outlined-required"
-              label="Nom Complet"
-              defaultValue="Nom Complet"
-            />
-            <TextField
-              required
-              id="outlined-required"
-              label="Adresse pharmacie"
-              defaultValue="Adresse pharmacie"
-            />
-            <TextField
-              className="textfield"
-              id="outlined-number"
-              label="Number"
-              type="number"
-              InputLabelProps={{
-                shrink: true,
-              }}
-            />
-            <TextField
-              className="textfield"
-              id="date"
-              label="Date Livraison"
-              type="date"
-              defaultValue="2017-05-24"
-              InputLabelProps={{
-                shrink: true,
-              }}
-            />
-          </div>
-
-          <div className="right">
-            <TextField
-              required
-              id="outlined-required"
-              label="Pharmacie"
-              defaultValue="Nom pharmacie"
-            />
-
-            <TextField
-              className="textfield"
-              required
-              id="outlined-required"
-              label="Nom Médicaments"
-              defaultValue="Nom Médicaments"
-            />
-
-            <TextField
-              className="textfield"
-              id="date"
-              label="Date Commande"
-              type="date"
-              defaultValue="2017-05-24"
-              InputLabelProps={{
-                shrink: true,
-              }}
-            />
-
-            <FormControl className="textfield">
-              <FormLabel id="demo-row-radio-buttons-group-label">
-                Situation
-              </FormLabel>
-              <RadioGroup
-                row
-                aria-labelledby="demo-row-radio-buttons-group-label"
-                name="row-radio-buttons-group"
-              >
-                <FormControlLabel
-                  value="female"
-                  control={<Radio />}
-                  label="Urgent"
-                />
-                <FormControlLabel
-                  value="male"
-                  control={<Radio />}
-                  label="Pas Urgent"
-                />
-              </RadioGroup>
-            </FormControl>
-          </div>
-
-          <Stack direction="row" spacing={3}>
-            <Button variant="contained" style={{backgroundColor:'#3ecfa3' ,marginTop:'100px', display:'block', margin:'auto'}}>Commander</Button>
-          </Stack>
+      <div className="title"> Commande ici !</div>
+      <div className="form-area">
+        <div className="left">
+          
+          <TextField
+            className="textfield"
+            required
+            id="outlined-required"
+            label="Nom Complet"
+          />
+          <TextField
+            required
+            id="outlined-required"
+            label="Adresse pharmacie"
+          />
+          <TextField
+            className="textfield"
+            id="outlined-number"
+            label="Quantité"
+            type="number"
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+          <TextField
+            className="textfield"
+            id="date"
+            label="Date Livraison"
+            type="date"
+            defaultValue="2017-05-24"
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
         </div>
+        <div className="right">
+          <TextField
+            className="textfield"
+            required
+            id="outlined-required"
+            label="Nom Médicaments"
+          />
+          <TextField
+            className="textfield"
+            id="date"
+            label="Date Commande"
+            type="date"
+            defaultValue="2017-05-24"
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+          <TextField
+            required
+            id="outlined-required"
+            label="Pharmacie"
+          />
+
+          <FormControl className="textfield">
+            <FormLabel id="demo-row-radio-buttons-group-label">
+              Situation
+            </FormLabel>
+            <RadioGroup
+              row
+              aria-labelledby="demo-row-radio-buttons-group-label"
+              name="row-radio-buttons-group"
+            >
+              <FormControlLabel
+                value="female"
+                control={<Radio />}
+                label="Urgent"
+              />
+              <FormControlLabel
+                value="male"
+                control={<Radio />}
+                label="Pas Urgent"
+              />
+            </RadioGroup>
+          </FormControl>
+        </div>
+
       </div>
+
+      <Stack direction="row" spacing={3}>
+          <Button
+            variant="contained"
+            style={{
+              backgroundColor: "#3ecfa3",
+              marginTop: "80px",
+              display: "block",
+              margin: "auto",
+              fontSize:"17px"
+            }}
+          >
+            commander !{""}
+          </Button>
+        </Stack>
+      </div>
+     
+    </section>
     </Box>
   );
 }
+
+export default Form;
