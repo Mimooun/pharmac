7596ef1fd6c621d923f1311df845859f3d6dc65e
zakari64 <74094535+zakari64@users.noreply.github.com>
2022-03-10 21:01:04 +0100

    fixed signup

--- Client/src/components/Signup.jsx
+++ Client/src/components/Signup.jsx
@@ -2,61 +2,57 @@ import React from "react";
 import "../styles/signup.css";
 import TextField from "@mui/material/TextField";
 import Button from "@material-ui/core/Button";
-import {sos} from "../assets/images/SOSpharma2.png"
+import sos from "../assets/images/SOSpharma2.png";
+import {Link} from "react-router-dom"
 
 function Signup() {
   return (
-    <section className="signUp">
-    <div className="container__form">
-
-      <div className="inputs">
-      <div className="icon-container">
-          <img src={sos} alt="" />
+    <section className="signUp1">
+      <div className="container__form1">
+        <div className="inputz">
+          <div className="icon-container1">
+            <img src={sos} alt="" />
+          </div>
+          <TextField
+            fullWidth
+            size="small"
+            label="Firstname"
+            variant="outlined"
+          />
+          <TextField
+            fullWidth
+            autoComplete="current-password"
+            size="small"
+            label="Lastname"
+            variant="outlined"
+          />
+          <TextField
+            fullWidth
+            type="password"
+            autoComplete="current-password"
+            size="small"
+            label="Password"
+            variant="outlined"
+          />
+          <TextField
+            fullWidth
+            type="password"
+            autoComplete="current-password"
+            size="small"
+            label="Confirm Password"
+            variant="outlined"
+          />
         </div>
-        <TextField
-          fullWidth
-          size="small"
-          label="Firstname"
-          variant="outlined"
-        />
-        <TextField
-          fullWidth
-          autoComplete="current-password"
-          size="small"
-          label="Lastname"
-          variant="outlined"
-        />
-          <TextField
-          fullWidth
-          type="password"
-          autoComplete="current-password"
-          size="small"
-          label="Password"
-          variant="outlined"
-        />
-          <TextField
-          fullWidth
-          type="password"
-          autoComplete="current-password"
-          size="small"
-          label="Confirm Password"
-          variant="outlined"
-        />
+        <p>Forgot Password ?</p>
+        <div className="btn1">
+          <Link to="/">
+            <Button fullWidth disableElevation variant="contained">
+              Signup
+            </Button>
+          </Link>
+        </div>
       </div>
-      <p>Forgot Password ?</p>
-      <div className="btn">
-        <Button
-         
-          fullWidth
-          disableElevation
-          variant="contained"
-        >
-          Signup
-        </Button>
-        
-      </div>
-    </div>
-  </section>
+    </section>
   );
 }
 
