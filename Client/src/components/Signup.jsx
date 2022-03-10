296a360e1cda7b9c6397aa729bcff34f71837bb5
zakari64 <74094535+zakari64@users.noreply.github.com>
2022-03-10 20:37:24 +0100

    finished signup
    stuck with image

--- Client/src/components/Signup.jsx
+++ Client/src/components/Signup.jsx
@@ -1,22 +1,62 @@
 import React from "react";
 import "../styles/signup.css";
 import TextField from "@mui/material/TextField";
+import Button from "@material-ui/core/Button";
+import {sos} from "../assets/images/SOSpharma2.png"
 
 function Signup() {
   return (
-    <section>
-      <div className="area">
-        <div className="one">
-          <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-          <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-        </div>
-        <div className="two">
-        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+    <section className="signUp">
+    <div className="container__form">
 
+      <div className="inputs">
+      <div className="icon-container">
+          <img src={sos} alt="" />
         </div>
+        <TextField
+          fullWidth
+          size="small"
+          label="Firstname"
+          variant="outlined"
+        />
+        <TextField
+          fullWidth
+          autoComplete="current-password"
+          size="small"
+          label="Lastname"
+          variant="outlined"
+        />
+          <TextField
+          fullWidth
+          type="password"
+          autoComplete="current-password"
+          size="small"
+          label="Password"
+          variant="outlined"
+        />
+          <TextField
+          fullWidth
+          type="password"
+          autoComplete="current-password"
+          size="small"
+          label="Confirm Password"
+          variant="outlined"
+        />
       </div>
-    </section>
+      <p>Forgot Password ?</p>
+      <div className="btn">
+        <Button
+         
+          fullWidth
+          disableElevation
+          variant="contained"
+        >
+          Signup
+        </Button>
+        
+      </div>
+    </div>
+  </section>
   );
 }
 
