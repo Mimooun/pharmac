6612e6e2341940322d6750db34b53e6880319b60
zakari64 <74094535+zakari64@users.noreply.github.com>
2022-04-05 22:38:15 +0000

    added user stuff

--- Client/src/components/Formnav.jsx
+++ Client/src/components/Formnav.jsx
@@ -1,29 +1,44 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/formnav.css";
-import logo from "../assets/images/SOSpharma2.png";
-import chario from '../assets/images/shopping-cart.png'
+import doctor from "../assets/images/diagnosis.png";
+import chario from "../assets/images/shopping-cart.png";
 
 function Formnav() {
   return (
     <nav className="navbar">
       <div className="container">
-        <div className="logo">
-          <Link to="/">
-            <img src={logo} alt="" />
-          </Link>
+        <div className="usr-area">
+          <div className="content">
+            <img src={doctor} />
+            <div className="usr-text">
+              <span>Mimoun</span>
+            </div>
+          </div>
         </div>
         <div className="menu">
           <ul>
-            <Link to="/"><li>Home</li></Link>
-            <Link to=""><li>Shop</li></Link>
-            <Link to="/Listep"><li>Products</li></Link>
-            <Link to=""><li>Pages</li></Link>
-            <Link to=""><li>Blog</li></Link>
+            <Link to="/">
+              <li>Home</li>
+            </Link>
+            <Link to="">
+              <li>Shop</li>
+            </Link>
+            <Link to="/Listep">
+              <li>Products</li>
+            </Link>
+            <Link to="">
+              <li>Pages</li>
+            </Link>
+            <Link to="">
+              <li>Blog</li>
+            </Link>
           </ul>
         </div>
         <div className="cart">
-           <Link to="/panier"><img src={chario} alt="" /></Link>  
+          <Link to="/panier">
+            <img src={chario} alt="" />
+          </Link>
         </div>
       </div>
     </nav>
